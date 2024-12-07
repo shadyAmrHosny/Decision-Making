879b194c7df6fd4e753ea9acd57cefaf8267f912
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-12-07 22:59:31 +0200

    Create jwt-auth.guard.ts

---
+++ src/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,25 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+import { IS_PUBLIC_KEY } from '../../decorators/public.decorator';
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt'){
+  constructor(private reflector: Reflector) {
+    super();
+  }
+  canActivate(context: ExecutionContext) {
+    console.log('inside jwt auth guard');
+
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context)
+  }
+}
