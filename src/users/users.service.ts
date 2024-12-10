5495b1098b4f7a5ede34779d81ec483a6fc7d259
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-12-10 19:10:38 +0200

    encrypt users password while creating

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException,BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt'
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -8,8 +9,21 @@ import { CreateUserDto } from './dtos/create-user.dto';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-  create(createUserDto: CreateUserDto){
-    const user = this.repo.create(createUserDto);
+  async create(createUserDto: CreateUserDto){
+    const existingUser = await this.repo.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (existingUser) {
+      throw new BadRequestException(`Email ${createUserDto.email} is already in use`);
+    }
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+   // const user = this.repo.create(createUserDto);
+    const user = this.repo.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
 
     return this.repo.save(user);
   }
@@ -22,8 +36,8 @@ export class UsersService {
   async findAll() {
     return this.repo.find();
   }
-  find(email: string) {
-    return this.repo.find({ where: { email, is_active:true } });
+  async find(email: string){
+    return  this.repo.findOne({ where: { email, is_active:true } });
   }
 
   async update(id: number, attrs: Partial<User>){
