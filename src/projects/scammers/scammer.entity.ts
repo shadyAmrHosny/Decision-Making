ddbfa1d2f19f887e4745048639c98909882dfd1b
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-12-18 21:23:10 +0200

    Update scammer.entity.ts

--- src/projects/scammers/scammer.entity.ts
+++ src/projects/scammers/scammer.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column,} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class Scammer {
@@ -8,5 +8,12 @@ export class Scammer {
 
   @Column()
   name: string;
+
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 
 }
