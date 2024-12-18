889e944246d7637526ee11ff45abd6ad1cf59735
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-12-18 21:21:31 +0200

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,10 +2,12 @@ import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common'
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { Public } from '../decorators/public.decorator';
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
+  @Public()
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
@@ -33,6 +35,5 @@ export class UsersController {
   async remove(@Param('id') id: number) {
     return this.userService.remove(id);
   }
-
 
 }
